6c612b6b0a1b57d51fcd74b011b7f6b2d0946d27
moafaqs <moafaq>
2021-02-08 17:16:01 +0300

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <h1>Learn to design and code React Apps</h1>
+    <p>Complete courses about the best tools and design system</p>
+
+    <Link to="/page-2/">Go to page 2   </Link> <br />
   </Layout>
 )
 
